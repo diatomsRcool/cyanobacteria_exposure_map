3af2572450c3c114215791fb5df4d61a1fc80bc4
diatomsRcool <annethessen@gmail.com>
2023-07-12 14:51:53 -0400

    able to plot CO water

--- build_map.py
+++ build_map.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 
 CO = gpd.read_file("/Volumes/PCCOMP/Colorado shape files/Colorado_State_Boundary/Colorado_State_Boundary.shp")
 CO_zip = gpd.read_file("/Volumes/PCCOMP/Colorado shape files/Colorado_ZIP_Code_Tabulation_Areas_(ZCTA)/Colorado_ZIP_Code_Tabulation_Areas_(ZCTA).shp")
+CO_water = gpd.read_file("/Volumes/PCCOMP/Colorado shape files/LAKES/LAKES.shp")
+water = CO_water.to_crs(CO_zip.crs)
 
-base = CO.plot(color='white', edgecolor='black')
+base = CO_zip.plot(color='white', edgecolor='black')
+#base = CO.plot(color='white', edgecolor='black')
 
-CO_zip.plot(ax=base)
+water.plot(ax=base)
+#CO_zip.plot(ax=base)
 plt.show()
