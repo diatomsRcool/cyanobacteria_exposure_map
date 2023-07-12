b43508abdb8a70a29d6b85995f62525ddc84868b
diatomsRcool <annethessen@gmail.com>
2023-07-12 14:29:55 -0400

    able to map CO zip codes

--- build_map.py
+++ build_map.py
@@ -2,6 +2,9 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 CO = gpd.read_file("/Volumes/PCCOMP/Colorado shape files/Colorado_State_Boundary/Colorado_State_Boundary.shp")
+CO_zip = gpd.read_file("/Volumes/PCCOMP/Colorado shape files/Colorado_ZIP_Code_Tabulation_Areas_(ZCTA)/Colorado_ZIP_Code_Tabulation_Areas_(ZCTA).shp")
 
-CO.plot()
+base = CO.plot(color='white', edgecolor='black')
+
+CO_zip.plot(ax=base)
 plt.show()
